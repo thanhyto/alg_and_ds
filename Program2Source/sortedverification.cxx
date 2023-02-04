ca1c3b5ee57de43472f78fe7cf1f833d8bc83b4c
Thanh To <thanhyto.per@gmail.com>
2023-02-03 17:35:14 -0800

    add comments executable 1

--- Program2Source/sortedverification.cxx
+++ Program2Source/sortedverification.cxx
@@ -18,36 +18,36 @@ int main(int argc, char** argv) {
     nlohmann::json jsonOutput;
     // Use the same metadata of the sample file
     jsonOutput["metadata"] = data["metadata"];
+    // vector<int> sortSample = data["Sample1"].get<vector<int>>();
     // Get the size of the array and the number of samples
     int arrSize = data["metadata"]["arraySize"]; // size of the array
     int numSamp = data["metadata"]["numSamples"]; // number of samples
     int inversion = 0; //number of inversion in 1 sample
     int invCount = 0; //number of samples with inversions
-    vector<string> sampleName;
+    vector<string> sampleName; //create a vector to store the samples
+    //Loops over the JSON file to store the name of the sample
     for(auto it = data.begin(); it != data.end(); ++it){
-        if(it.key() != "metadata")
-            cout << it.key() << it.value() << endl;
+        if(it.key() != "metadata") //Except the metadata key
             sampleName.push_back(it.key());
     }
-    for(int i = 0; i < numSamp; i++){
-        if(inversion != 0)
-            invCount += 1;
-        inversion = 0;
-        int min = data[sampleName[i]][0];
-
+    //Begin looping to check for the sorted array
+    for(int i = 0; i < numSamp; i++){ //Takes in the name of the sample
         for(int j = 0; j < arrSize; j++){
-            if(data[sampleName[i]][j] < min)
-                cout << min << " " << data[sampleName[i]][j] << endl;
-                min = data[sampleName[i]][j];
-                inversion +=1;
-                jsonOutput[sampleName[i]] = "ConsecutiveInversions";
-                // jsonOutput[sampleName[i]]["ConsecutiveInversions"][to_string(inversion)]= {min, data[sampleName[i]][j]};
-
+            if( data[sampleName[i]][j] > data[sampleName[i]][j+1]){ //if A[j] < A[j+1]
+                jsonOutput[sampleName[i]]["ConsecutiveInversions"][to_string(j)]={data[sampleName[i]][j],data[sampleName[i]][j+1]}; //Create "ConsecutiveInversions" key and assign the value of A[j] and A[j+1]
+                inversion += 1; //Count the number of inversion in this array only
+                jsonOutput[sampleName[i]]["sample"] = data[sampleName[i]];//Takes in the values of the array
+            }
         }
-        
+
+        if(inversion != 0){
+            invCount +=1; //count the total inversion in a file
+            inversion = 0; //set the inversion of 1 array back to 0
+        }
     }
 
-    jsonOutput["metadata"]["samplesWithInversions"] = invCount;
-    cout << jsonOutput << endl;
+    
+    jsonOutput["metadata"]["samplesWithInversions"] = invCount; //assign "samplesWithInversions" with invCount after looping
+    cout << jsonOutput << endl; //output the JSON
     return 1;
 }
